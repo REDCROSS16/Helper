cb299bc4d546f97002779c5c952f7fe47de5e623
REDCROSS16 <belkill@mail.ru>
2023-01-04 22:33:01 +0300

    update helper

--- src/Controller/Excel/ReadExcelFileAction.php
+++ src/Controller/Excel/ReadExcelFileAction.php
@@ -4,21 +4,37 @@ namespace App\Controller\Excel;
 
 use Box\Spout\Common\Entity\Row;
 use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+use Box\Spout\Reader\XLSX\Sheet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ReadExcelFileAction extends AbstractController
 {
-    public function __invoke(): \Symfony\Component\HttpFoundation\JsonResponse
+    public const MAX_FILE_SIZE = 30000000;
+
+    #[Route('/excel/read', name: 'excel_file_reader')]
+    public function __invoke(string $dir): \Symfony\Component\HttpFoundation\JsonResponse
     {
-        $file  = 'path';
-        $spout = ReaderEntityFactory::createReaderFromFile($file);
+        $path  = $dir . DIRECTORY_SEPARATOR . 'WY_1.xlsx';
+        $file  = new File($path);
+
+        $size = $file->getSize();
+
+        if ($size > self::MAX_FILE_SIZE) {
+            return $this->json('File is too big, filesize: ' . $size);
+        }
+
+        $spout = ReaderEntityFactory::createReaderFromFile($path);
         $spout->open($file);
 
+        // перебираем страницы нашего файла
         foreach ($spout->getSheetIterator() as $sheet) {
-            foreach ($sheet as $row) {
+            /** @var Sheet $sheet */
+            foreach ($sheet->getRowIterator() as $row) {
                 /** @var Row $row */
                 $cells = $row->getCells();
-                dd($cells);
+                \print_r($cells);
             }
         }
 
